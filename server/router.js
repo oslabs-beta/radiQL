501a840986652cc2eb17097c365e40be74895755
rjiang12 <roy.jiang.4757@gmail.com>
2022-08-01 11:45:22 -0700

    push
    Co-authored-by: t1ho <t1ho@users.noreply.github.com>

---
+++ server/router.js
@@ -0,0 +1,11 @@
+/*
+functionality required:
+input: database
+schema generation based on table row types
+resolver generation based on schema?
+*/
+const express = require('express'); 
+const router = express.Router(); 
+const path = require('path'); 
+
+module.exports = router; 

c88e3757f7816f44682eb4bdbc1a3b801c8901a8
Alex-Cusick <Alex_Cusick@live.com>
2022-08-01 14:44:22 -0400

    Adding background
    Co-authored-by: Jordan Williams <JordanOBL@users.noreply.github.com>
    Co-authored-by: Zachrobdev <Zachrobdev@users.noreply.github.com>

--- client/React/main.tsx
+++ client/React/main.tsx
@@ -5,10 +5,12 @@ import axios from 'axios';
 
 const Main = props => {
 
-  
+
 
   return (
     <div>
+      <div id="bg-L"></div>
+      <div id="bg-R"></div>
       <h1 className='App-title'>radiQL</h1>
       < NavBar /> 
     </div>

1e47ed2349e829e1a69c949f6f76d4dd2ecba522
Alex-Cusick <Alex_Cusick@live.com>
2022-08-03 17:01:58 -0400

    Finished About page outline

--- client/React/NavBar.tsx
+++ client/React/NavBar.tsx
@@ -14,27 +14,27 @@ const NavBar = props => {
       <motion.div
       className="box"
       animate={{
-        scale: [1, 1.1, 1],
-        rotate: [0, 10, 0 -10, 0],
+        scale: [1, 1, 1],
+        rotate: [0, 8, 0 -8, 0],
       }}
       transition={{
         duration: .5,
         ease: "easeInOut",
         times: [0, 1],
         repeat: Infinity,
-        repeatDelay: 10
+        repeatDelay: 15
       }}
       >
-        <a id="logo" className="nav-link" href="/">
+        <Link id="logo" className="nav-link" to="/">
           <img id="logo" src={logo} />
-        </a>
+        </Link>
       </motion.div>
 
       <div className="links">
         <Link className="nav-link" to="/About">About Us</Link>
         <a className="nav-link" href="https://medium.com/">Medium Article</a>
         <a className="nav-link" href="https://github.com/oslabs-beta/radiQL">Github</a>
-        <a id="login" onClick={()=> setShowLogin(!showLogin)} className="nav-link" href="#">Login</a>
+        <a id="login" onClick={()=> setShowLogin(!showLogin)} className="nav-link">Login</a>
         <motion.div>
           <AnimatePresence>
             {showLogin && (

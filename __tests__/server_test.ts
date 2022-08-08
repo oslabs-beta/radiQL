34b9a19904e044f038234a7441601ee0b5c6f31d
rjiang12 <roy.jiang.4757@gmail.com>
2022-08-08 13:44:50 -0700

    typing

--- __tests__/server_test.ts
+++ __tests__/server_test.ts
@@ -1,23 +1,23 @@
 import request from 'supertest'; 
 import { app } from '../server/server'; 
 
-describe("Test server.ts", () => {
+describe("Test server.ts", (): void => {
   test("Catch nonexistent route with 404", async () => {
     const res = await request(app).get("/404");
     expect(res.status).toEqual(404);
   });
   test("Catch incorrect request method", async () => {
-    const res = await request(app).get("/submitURI");
+    const res: request.Response = await request(app).get("/submitURI");
     expect(res.status).toEqual(404); 
   });
   test("Can receive a URI", async () => {
-    const res = await request(app).post("/submitURI")
+    const res: request.Response = await request(app).post("/submitURI")
     .set('Content-type', 'application/json')
     .send({ dbURI: "postgres://mbvnsdqx:Saf3Rk2qSOmYrab1SzA35utIB5s0jxCQ@heffalump.db.elephantsql.com/mbvnsdqx"});
     expect(res.status).toEqual(200); 
   });
   test("Can receive a URI and return an appropriate object", async () => {
-    const res = await request(app).post("/submitURI")
+    const res: request.Response = await request(app).post("/submitURI")
     .set('Content-type', 'application/json')
     .send({ dbURI: "postgres://rcwvxixj:QoMsDTMRQyrIxkI_ZfWPvUvHwdifXLt4@heffalump.db.elephantsql.com/rcwvxixj"});
     expect(res.status).toEqual(200); 
@@ -25,9 +25,9 @@ describe("Test server.ts", () => {
     expect(res.text).toContain('"resolver":');
   });
   test("Can allow a registered user to log in", async () => {
-    const res = await request(app).post("/login")
+    const res: request.Response = await request(app).post("/login")
     .set('Content-type', 'application/json')
     .send({ username: "test666", password: "test"});
     expect(res.status).toEqual(200);
-  })
+  });
 });

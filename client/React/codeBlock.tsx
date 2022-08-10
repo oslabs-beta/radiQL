de45d8984dd508bba422d825e7548ed22561e9f2
Alex-Cusick <Alex_Cusick@live.com>
2022-08-10 16:29:46 -0400

    Refactored Sass files to import from _variables

--- client/React/codeBlock.tsx
+++ client/React/codeBlock.tsx
@@ -78,7 +78,6 @@ const CodeBlock = ({schemaBody, resolverBody, setInstruction, currentTab, change
           <button className={ currentTab === 3 ? '' : 'not-active' } onClick={() => changeTab(3)}>Diagram</button>
         </section>
         <form className="mx-10">
-          <label>Include boilerplate code</label>
           <select title='boilerplatecode' className="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
             <option value="No boilerplate code">No boilerplate code</option>
             <option value="GraphQL.js">GraphQL.js</option>

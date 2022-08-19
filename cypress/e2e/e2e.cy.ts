199a9e2df2bc0dd85f10015903a00381c23f9615
rjiang12 <roy.jiang.4757@gmail.com>
2022-08-19 16:41:48 -0700

    e2e w/ cypress implemented

---
+++ cypress/e2e/e2e.cy.ts
@@ -0,0 +1,20 @@
+
+describe('empty spec', () => {
+  it('can login and logout', () => {
+    //login
+    cy.visit('http://localhost:8080/');
+    cy.wait(5000);
+    cy.contains('Login').click();
+    cy.contains('Login');
+    cy.contains('Register?'); 
+    cy.get('#login-username').type('test666');
+    cy.get('#login-password').type('test');
+    cy.get('#login-btn').click();
+
+    //logout
+    cy.contains('My Account').click();
+    cy.contains('Logged into account: test666');
+    cy.contains('Logout').click();
+    cy.contains('Login');
+  });
+})
